7f912af01f85a3884346b68e5aa43f2e04c5fce5
Jeremy D. Frens <jdfrens@gmail.com>
2004-11-19 21:50:46 +0000

    Created.

---
+++ Vitae/auto/myvita.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook "myvita"
+ (function
+  (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     "vita10"
+     "vita"
+     "ComputerScience"))))
+
---
+++ Vitae/auto/vitatest.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook "vitatest"
+ (function
+  (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     "vita10"
+     "vita"
+     "ComputerScience"))))
+
