5aad0f3f080c213f952a9774e55459fbe4fba5c2
Jeremy D. Frens <jdfrens@gmail.com>
2005-05-31 17:44:50 +0000

    Created research plan, finished first partial draft.

--- Vitae/auto/myvita.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook "myvita"
- (function
-  (lambda ()
-    (TeX-run-style-hooks
-     "latex2e"
-     "vita10"
-     "vita"
-     "ComputerScience"))))
-
--- Vitae/auto/vitatest.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook "vitatest"
- (function
-  (lambda ()
-    (TeX-run-style-hooks
-     "latex2e"
-     "vita10"
-     "vita"
-     "ComputerScience"))))
-
